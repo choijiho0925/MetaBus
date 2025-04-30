d6d17b5f5fa55577dd3909169f46facdcb54ed81
choijiho0925 <jiho.0925.choi@gmail.com>
2025-05-01 01:42:05 +0900

    feat : FlapPlane 종료창에 현재점수 추가
    현재점수 추가

--- Assets/FlapPlane/Scripts/GameUIManager.cs
+++ Assets/FlapPlane/Scripts/GameUIManager.cs
@@ -7,10 +7,15 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using static UnityEditor.ShaderData;
+
 public class GameUIManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameScoreText;
+    public TextMeshProUGUI bestGameScore;
+
     public Image restartPanel;
+
     public GameObject descriptionPanel;
 
     public static bool isPlaying = true;
@@ -21,7 +26,7 @@ public class GameUIManager : MonoBehaviour
         {
             descriptionPanel.SetActive(true);
         } 
-        restartPanel.gameObject.SetActive(false);    
+        restartPanel.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -41,5 +46,6 @@ public class GameUIManager : MonoBehaviour
     public void UpdateScore(int score)
     {
         scoreText.text = score.ToString();
+        gameScoreText.text = score.ToString();
     }
 }
