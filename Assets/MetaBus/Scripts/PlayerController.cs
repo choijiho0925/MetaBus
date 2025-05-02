25c0f181dacff22da2e08be5886196964845cbd7
choijiho0925 <jiho.0925.choi@gmail.com>
2025-05-02 18:04:13 +0900

    update : Metabus 방

--- Assets/MetaBus/Scripts/PlayerController.cs
+++ Assets/MetaBus/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : BaseController
+public class PlayerController : BaseController
 {
     protected override void Start()
     {
