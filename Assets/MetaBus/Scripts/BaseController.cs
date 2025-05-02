6dbf13d66a2d518d269b68a47a64371a1c18357b
choijiho0925 <jiho.0925.choi@gmail.com>
2025-05-02 17:27:20 +0900

    feat : Character 애니메이션

--- Assets/MetaBus/Scripts/BaseController.cs
+++ Assets/MetaBus/Scripts/BaseController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;
+    protected AnimationHandler animationHandler;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero; // 현재 바라보는 방향
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    protected void Movement(Vector2 direction)
+    {
+        direction = direction * 5.0f; 
+
+        _rigidbody.velocity = direction;
+        animationHandler.Move(direction);
+    }
+
+    protected void Rotate(Vector2 direction)
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (horizontal > 0)
+        {
+            characterRenderer.flipX = false; 
+        }
+        else if (horizontal < 0)
+        {
+            characterRenderer.flipX = true; 
+        }
     }
 }
